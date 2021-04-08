b019680a99d79218f46c74f9ee699a641212ba37
Marvin Countryman <me@maar.vin>
2021-04-08 18:58:38 -0400

    fix: resolve builds not working on non x86 x86_64 arch targets

--- src/imp/avx2.rs
+++ src/imp/avx2.rs
@@ -1,162 +1,162 @@
-const MOD: u32 = 65521;
-const NMAX: usize = 5552;
-const BLOCK_SIZE: usize = 64;
-const CHUNK_SIZE: usize = NMAX / BLOCK_SIZE * BLOCK_SIZE;
-
 use super::Adler32Imp;
-
-#[cfg(target_arch = "x84")]
-use core::arch::x86::*;
-#[cfg(target_arch = "x86_64")]
-use core::arch::x86_64::*;
 
 /// Resolves update implementation if CPU supports avx2 instructions.
 pub fn get_imp() -> Option<Adler32Imp> {
   #[cfg(all(feature = "std", target_arch = "x86"))]
   if std::is_x86_feature_detected!("avx2") {
-    return Some(update);
+    return Some(imp::update);
   }
 
   #[cfg(all(feature = "std", target_arch = "x86_64"))]
   if std::is_x86_feature_detected!("avx2") {
-    return Some(update);
+    return Some(imp::update);
   }
 
-  cfg_if::cfg_if! {
-    if #[cfg(target_feature = "avx2")] {
-      Some(update)
-    } else {
-      None
+  #[cfg(target_feature = "avx2")]
+  return Some(imp::update);
+
+  None
+}
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+mod imp {
+  const MOD: u32 = 65521;
+  const NMAX: usize = 5552;
+  const BLOCK_SIZE: usize = 64;
+  const CHUNK_SIZE: usize = NMAX / BLOCK_SIZE * BLOCK_SIZE;
+
+  #[cfg(target_arch = "x84")]
+  use core::arch::x86::*;
+  #[cfg(target_arch = "x86_64")]
+  use core::arch::x86_64::*;
+
+  pub fn update(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
+    unsafe { update_imp(a, b, data) }
+  }
+
+  #[inline]
+  #[target_feature(enable = "avx2")]
+  unsafe fn update_imp(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
+    let mut a = a as u32;
+    let mut b = b as u32;
+
+    let chunks = data.chunks_exact(CHUNK_SIZE);
+    let remainder = chunks.remainder();
+    for chunk in chunks {
+      update_chunk_block(&mut a, &mut b, chunk);
     }
-  }
-}
 
-fn update(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
-  unsafe { update_imp(a, b, data) }
-}
+    update_block(&mut a, &mut b, remainder);
 
-#[inline]
-#[target_feature(enable = "avx2")]
-unsafe fn update_imp(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
-  let mut a = a as u32;
-  let mut b = b as u32;
-
-  let chunks = data.chunks_exact(CHUNK_SIZE);
-  let remainder = chunks.remainder();
-  for chunk in chunks {
-    update_chunk_block(&mut a, &mut b, chunk);
+    (a as u16, b as u16)
   }
 
-  update_block(&mut a, &mut b, remainder);
+  #[inline]
+  unsafe fn update_chunk_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
+    debug_assert_eq!(
+      chunk.len(),
+      CHUNK_SIZE,
+      "Unexpected chunk size (expected {}, got {})",
+      CHUNK_SIZE,
+      chunk.len()
+    );
 
-  (a as u16, b as u16)
-}
+    reduce_add_blocks(a, b, chunk);
 
-#[inline]
-unsafe fn update_chunk_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
-  debug_assert_eq!(
-    chunk.len(),
-    CHUNK_SIZE,
-    "Unexpected chunk size (expected {}, got {})",
-    CHUNK_SIZE,
-    chunk.len()
-  );
-
-  reduce_add_blocks(a, b, chunk);
-
-  *a %= MOD;
-  *b %= MOD;
-}
-
-#[inline]
-unsafe fn update_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
-  debug_assert!(
-    chunk.len() <= CHUNK_SIZE,
-    "Unexpected chunk size (expected <= {}, got {})",
-    CHUNK_SIZE,
-    chunk.len()
-  );
-
-  for byte in reduce_add_blocks(a, b, chunk) {
-    *a += *byte as u32;
-    *b += *a;
+    *a %= MOD;
+    *b %= MOD;
   }
 
-  *a %= MOD;
-  *b %= MOD;
-}
+  #[inline]
+  unsafe fn update_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
+    debug_assert!(
+      chunk.len() <= CHUNK_SIZE,
+      "Unexpected chunk size (expected <= {}, got {})",
+      CHUNK_SIZE,
+      chunk.len()
+    );
 
-#[inline(always)]
-unsafe fn reduce_add_blocks<'a>(a: &mut u32, b: &mut u32, chunk: &'a [u8]) -> &'a [u8] {
-  if chunk.len() < BLOCK_SIZE {
-    return chunk;
+    for byte in reduce_add_blocks(a, b, chunk) {
+      *a += *byte as u32;
+      *b += *a;
+    }
+
+    *a %= MOD;
+    *b %= MOD;
   }
 
-  let blocks = chunk.chunks_exact(BLOCK_SIZE);
-  let blocks_remainder = blocks.remainder();
+  #[inline(always)]
+  unsafe fn reduce_add_blocks<'a>(a: &mut u32, b: &mut u32, chunk: &'a [u8]) -> &'a [u8] {
+    if chunk.len() < BLOCK_SIZE {
+      return chunk;
+    }
 
-  let one_v = _mm256_set1_epi16(1);
-  let zero_v = _mm256_set1_epi16(0);
-  let weight_hi_v = get_weight_hi();
-  let weight_lo_v = get_weight_lo();
+    let blocks = chunk.chunks_exact(BLOCK_SIZE);
+    let blocks_remainder = blocks.remainder();
 
-  let mut p_v = _mm256_set_epi32(0, 0, 0, 0, 0, 0, 0, (*a * blocks.len() as u32) as _);
-  let mut a_v = _mm256_set_epi32(0, 0, 0, 0, 0, 0, 0, 0);
-  let mut b_v = _mm256_set_epi32(0, 0, 0, 0, 0, 0, 0, *b as _);
+    let one_v = _mm256_set1_epi16(1);
+    let zero_v = _mm256_set1_epi16(0);
+    let weight_hi_v = get_weight_hi();
+    let weight_lo_v = get_weight_lo();
 
-  for block in blocks {
-    let block_ptr = block.as_ptr() as *const _;
+    let mut p_v = _mm256_set_epi32(0, 0, 0, 0, 0, 0, 0, (*a * blocks.len() as u32) as _);
+    let mut a_v = _mm256_set_epi32(0, 0, 0, 0, 0, 0, 0, 0);
+    let mut b_v = _mm256_set_epi32(0, 0, 0, 0, 0, 0, 0, *b as _);
 
-    let left_v = _mm256_loadu_si256(block_ptr);
-    let right_v = _mm256_loadu_si256(block_ptr.add(1));
+    for block in blocks {
+      let block_ptr = block.as_ptr() as *const _;
 
-    p_v = _mm256_add_epi32(p_v, a_v);
+      let left_v = _mm256_loadu_si256(block_ptr);
+      let right_v = _mm256_loadu_si256(block_ptr.add(1));
 
-    a_v = _mm256_add_epi32(a_v, _mm256_sad_epu8(left_v, zero_v));
-    let mad = _mm256_maddubs_epi16(left_v, weight_hi_v);
-    b_v = _mm256_add_epi32(b_v, _mm256_madd_epi16(mad, one_v));
+      p_v = _mm256_add_epi32(p_v, a_v);
 
-    a_v = _mm256_add_epi32(a_v, _mm256_sad_epu8(right_v, zero_v));
-    let mad = _mm256_maddubs_epi16(right_v, weight_lo_v);
-    b_v = _mm256_add_epi32(b_v, _mm256_madd_epi16(mad, one_v));
+      a_v = _mm256_add_epi32(a_v, _mm256_sad_epu8(left_v, zero_v));
+      let mad = _mm256_maddubs_epi16(left_v, weight_hi_v);
+      b_v = _mm256_add_epi32(b_v, _mm256_madd_epi16(mad, one_v));
+
+      a_v = _mm256_add_epi32(a_v, _mm256_sad_epu8(right_v, zero_v));
+      let mad = _mm256_maddubs_epi16(right_v, weight_lo_v);
+      b_v = _mm256_add_epi32(b_v, _mm256_madd_epi16(mad, one_v));
+    }
+
+    b_v = _mm256_add_epi32(b_v, _mm256_slli_epi32(p_v, 6));
+
+    *a += reduce_add(a_v);
+    *b = reduce_add(b_v);
+
+    blocks_remainder
   }
 
-  b_v = _mm256_add_epi32(b_v, _mm256_slli_epi32(p_v, 6));
+  #[inline(always)]
+  unsafe fn reduce_add(v: __m256i) -> u32 {
+    let sum = _mm_add_epi32(_mm256_castsi256_si128(v), _mm256_extracti128_si256(v, 1));
+    let hi = _mm_unpackhi_epi64(sum, sum);
 
-  *a += reduce_add(a_v);
-  *b = reduce_add(b_v);
+    let sum = _mm_add_epi32(hi, sum);
+    let hi = _mm_shuffle_epi32(sum, crate::imp::_MM_SHUFFLE(2, 3, 0, 1));
 
-  blocks_remainder
-}
+    let sum = _mm_add_epi32(sum, hi);
+    let sum = _mm_cvtsi128_si32(sum) as _;
 
-#[inline(always)]
-unsafe fn reduce_add(v: __m256i) -> u32 {
-  let sum = _mm_add_epi32(_mm256_castsi256_si128(v), _mm256_extracti128_si256(v, 1));
-  let hi = _mm_unpackhi_epi64(sum, sum);
+    sum
+  }
 
-  let sum = _mm_add_epi32(hi, sum);
-  let hi = _mm_shuffle_epi32(sum, super::_MM_SHUFFLE(2, 3, 0, 1));
+  #[inline(always)]
+  unsafe fn get_weight_lo() -> __m256i {
+    _mm256_set_epi8(
+      1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+      24, 25, 26, 27, 28, 29, 30, 31, 32,
+    )
+  }
 
-  let sum = _mm_add_epi32(sum, hi);
-  let sum = _mm_cvtsi128_si32(sum) as _;
-
-  sum
-}
-
-#[inline(always)]
-unsafe fn get_weight_lo() -> __m256i {
-  _mm256_set_epi8(
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-    24, 25, 26, 27, 28, 29, 30, 31, 32,
-  )
-}
-
-#[inline(always)]
-unsafe fn get_weight_hi() -> __m256i {
-  _mm256_set_epi8(
-    33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
-    54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
-  )
+  #[inline(always)]
+  unsafe fn get_weight_hi() -> __m256i {
+    _mm256_set_epi8(
+      33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53,
+      54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64,
+    )
+  }
 }
 
 #[cfg(test)]
@@ -201,7 +201,7 @@ mod tests {
   }
 
   fn assert_sum_eq(data: &[u8]) {
-    let (a, b) = super::update(1, 0, data);
+    let (a, b) = super::imp::update(1, 0, data);
     let left = u32::from(b) << 16 | u32::from(a);
     let right = adler::adler32_slice(data);
 
--- src/imp/ssse3.rs
+++ src/imp/ssse3.rs
@@ -1,153 +1,153 @@
-const MOD: u32 = 65521;
-const NMAX: usize = 5552;
-const BLOCK_SIZE: usize = 32;
-const CHUNK_SIZE: usize = NMAX / BLOCK_SIZE * BLOCK_SIZE;
-
 use super::Adler32Imp;
-
-#[cfg(target_arch = "x84")]
-use core::arch::x86::*;
-#[cfg(target_arch = "x86_64")]
-use core::arch::x86_64::*;
 
 /// Resolves update implementation if CPU supports ssse3 instructions.
 pub fn get_imp() -> Option<Adler32Imp> {
   #[cfg(all(feature = "std", target_arch = "x86"))]
   if std::is_x86_feature_detected!("ssse3") {
-    return Some(update);
+    return Some(imp::update);
   }
 
   #[cfg(all(feature = "std", target_arch = "x86_64"))]
   if std::is_x86_feature_detected!("ssse3") {
-    return Some(update);
+    return Some(imp::update);
   }
 
-  cfg_if::cfg_if! {
-    if #[cfg(target_feature = "ssse3")] {
-      Some(update)
-    } else {
-      None
+  #[cfg(target_feature = "ssse3")]
+  return Some(imp::update);
+
+  None
+}
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+mod imp {
+  const MOD: u32 = 65521;
+  const NMAX: usize = 5552;
+  const BLOCK_SIZE: usize = 32;
+  const CHUNK_SIZE: usize = NMAX / BLOCK_SIZE * BLOCK_SIZE;
+
+  #[cfg(target_arch = "x84")]
+  use core::arch::x86::*;
+  #[cfg(target_arch = "x86_64")]
+  use core::arch::x86_64::*;
+
+  pub fn update(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
+    unsafe { update_imp(a, b, data) }
+  }
+
+  #[inline]
+  #[target_feature(enable = "ssse3")]
+  unsafe fn update_imp(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
+    let mut a = a as u32;
+    let mut b = b as u32;
+
+    let chunks = data.chunks_exact(CHUNK_SIZE);
+    let remainder = chunks.remainder();
+    for chunk in chunks {
+      update_chunk_block(&mut a, &mut b, chunk);
     }
-  }
-}
 
-fn update(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
-  unsafe { update_imp(a, b, data) }
-}
+    update_block(&mut a, &mut b, remainder);
 
-#[inline]
-#[target_feature(enable = "ssse3")]
-unsafe fn update_imp(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
-  let mut a = a as u32;
-  let mut b = b as u32;
-
-  let chunks = data.chunks_exact(CHUNK_SIZE);
-  let remainder = chunks.remainder();
-  for chunk in chunks {
-    update_chunk_block(&mut a, &mut b, chunk);
+    (a as u16, b as u16)
   }
 
-  update_block(&mut a, &mut b, remainder);
+  unsafe fn update_chunk_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
+    debug_assert_eq!(
+      chunk.len(),
+      CHUNK_SIZE,
+      "Unexpected chunk size (expected {}, got {})",
+      CHUNK_SIZE,
+      chunk.len()
+    );
 
-  (a as u16, b as u16)
-}
+    reduce_add_blocks(a, b, chunk);
 
-unsafe fn update_chunk_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
-  debug_assert_eq!(
-    chunk.len(),
-    CHUNK_SIZE,
-    "Unexpected chunk size (expected {}, got {})",
-    CHUNK_SIZE,
-    chunk.len()
-  );
-
-  reduce_add_blocks(a, b, chunk);
-
-  *a %= MOD;
-  *b %= MOD;
-}
-
-unsafe fn update_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
-  debug_assert!(
-    chunk.len() <= CHUNK_SIZE,
-    "Unexpected chunk size (expected <= {}, got {})",
-    CHUNK_SIZE,
-    chunk.len()
-  );
-
-  for byte in reduce_add_blocks(a, b, chunk) {
-    *a += *byte as u32;
-    *b += *a;
+    *a %= MOD;
+    *b %= MOD;
   }
 
-  *a %= MOD;
-  *b %= MOD;
-}
+  unsafe fn update_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
+    debug_assert!(
+      chunk.len() <= CHUNK_SIZE,
+      "Unexpected chunk size (expected <= {}, got {})",
+      CHUNK_SIZE,
+      chunk.len()
+    );
 
-#[inline(always)]
-unsafe fn reduce_add_blocks<'a>(a: &mut u32, b: &mut u32, chunk: &'a [u8]) -> &'a [u8] {
-  if chunk.len() < BLOCK_SIZE {
-    return chunk;
+    for byte in reduce_add_blocks(a, b, chunk) {
+      *a += *byte as u32;
+      *b += *a;
+    }
+
+    *a %= MOD;
+    *b %= MOD;
   }
 
-  let blocks = chunk.chunks_exact(BLOCK_SIZE);
-  let blocks_remainder = blocks.remainder();
+  #[inline(always)]
+  unsafe fn reduce_add_blocks<'a>(a: &mut u32, b: &mut u32, chunk: &'a [u8]) -> &'a [u8] {
+    if chunk.len() < BLOCK_SIZE {
+      return chunk;
+    }
 
-  let one_v = _mm_set1_epi16(1);
-  let zero_v = _mm_set1_epi16(0);
-  let weight_hi_v = get_weight_hi();
-  let weight_lo_v = get_weight_lo();
+    let blocks = chunk.chunks_exact(BLOCK_SIZE);
+    let blocks_remainder = blocks.remainder();
 
-  let mut p_v = _mm_set_epi32(0, 0, 0, (*a * blocks.len() as u32) as _);
-  let mut a_v = _mm_set_epi32(0, 0, 0, 0);
-  let mut b_v = _mm_set_epi32(0, 0, 0, *b as _);
+    let one_v = _mm_set1_epi16(1);
+    let zero_v = _mm_set1_epi16(0);
+    let weight_hi_v = get_weight_hi();
+    let weight_lo_v = get_weight_lo();
 
-  for block in blocks {
-    let block_ptr = block.as_ptr() as *const _;
-    let left_v = _mm_loadu_si128(block_ptr);
-    let right_v = _mm_loadu_si128(block_ptr.add(1));
+    let mut p_v = _mm_set_epi32(0, 0, 0, (*a * blocks.len() as u32) as _);
+    let mut a_v = _mm_set_epi32(0, 0, 0, 0);
+    let mut b_v = _mm_set_epi32(0, 0, 0, *b as _);
 
-    p_v = _mm_add_epi32(p_v, a_v);
+    for block in blocks {
+      let block_ptr = block.as_ptr() as *const _;
+      let left_v = _mm_loadu_si128(block_ptr);
+      let right_v = _mm_loadu_si128(block_ptr.add(1));
 
-    a_v = _mm_add_epi32(a_v, _mm_sad_epu8(left_v, zero_v));
-    let mad = _mm_maddubs_epi16(left_v, weight_hi_v);
-    b_v = _mm_add_epi32(b_v, _mm_madd_epi16(mad, one_v));
+      p_v = _mm_add_epi32(p_v, a_v);
 
-    a_v = _mm_add_epi32(a_v, _mm_sad_epu8(right_v, zero_v));
-    let mad = _mm_maddubs_epi16(right_v, weight_lo_v);
-    b_v = _mm_add_epi32(b_v, _mm_madd_epi16(mad, one_v));
+      a_v = _mm_add_epi32(a_v, _mm_sad_epu8(left_v, zero_v));
+      let mad = _mm_maddubs_epi16(left_v, weight_hi_v);
+      b_v = _mm_add_epi32(b_v, _mm_madd_epi16(mad, one_v));
+
+      a_v = _mm_add_epi32(a_v, _mm_sad_epu8(right_v, zero_v));
+      let mad = _mm_maddubs_epi16(right_v, weight_lo_v);
+      b_v = _mm_add_epi32(b_v, _mm_madd_epi16(mad, one_v));
+    }
+
+    b_v = _mm_add_epi32(b_v, _mm_slli_epi32(p_v, 5));
+
+    *a += reduce_add(a_v);
+    *b = reduce_add(b_v);
+
+    blocks_remainder
   }
 
-  b_v = _mm_add_epi32(b_v, _mm_slli_epi32(p_v, 5));
+  #[inline(always)]
+  unsafe fn reduce_add(v: __m128i) -> u32 {
+    let hi = _mm_unpackhi_epi64(v, v);
+    let sum = _mm_add_epi32(hi, v);
+    let hi = _mm_shuffle_epi32(sum, super::_MM_SHUFFLE(2, 3, 0, 1));
 
-  *a += reduce_add(a_v);
-  *b = reduce_add(b_v);
+    let sum = _mm_add_epi32(sum, hi);
+    let sum = _mm_cvtsi128_si32(sum) as _;
 
-  blocks_remainder
-}
+    sum
+  }
 
-#[inline(always)]
-unsafe fn reduce_add(v: __m128i) -> u32 {
-  let hi = _mm_unpackhi_epi64(v, v);
-  let sum = _mm_add_epi32(hi, v);
-  let hi = _mm_shuffle_epi32(sum, super::_MM_SHUFFLE(2, 3, 0, 1));
+  #[inline(always)]
+  unsafe fn get_weight_lo() -> __m128i {
+    _mm_set_epi8(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
+  }
 
-  let sum = _mm_add_epi32(sum, hi);
-  let sum = _mm_cvtsi128_si32(sum) as _;
-
-  sum
-}
-
-#[inline(always)]
-unsafe fn get_weight_lo() -> __m128i {
-  _mm_set_epi8(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16)
-}
-
-#[inline(always)]
-unsafe fn get_weight_hi() -> __m128i {
-  _mm_set_epi8(
-    17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
-  )
+  #[inline(always)]
+  unsafe fn get_weight_hi() -> __m128i {
+    _mm_set_epi8(
+      17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32,
+    )
+  }
 }
 
 #[cfg(test)]
@@ -192,7 +192,7 @@ mod tests {
   }
 
   fn assert_sum_eq(data: &[u8]) {
-    let (a, b) = super::update(1, 0, data);
+    let (a, b) = super::imp::update(1, 0, data);
     let left = u32::from(b) << 16 | u32::from(a);
     let right = adler::adler32_slice(data);
 
