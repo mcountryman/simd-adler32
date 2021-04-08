cbe32aa97dbc6fb4cf3475df8f510709b04c3495
Marvin Countryman <me@maar.vin>
2021-04-08 16:27:50 -0400

    docs: add benchmark results to README.md

--- bench/compete.rs
+++ bench/compete.rs
@@ -5,43 +5,58 @@ fn bench(c: &mut Criterion) {
   let mut random = vec![0; 100_000];
   thread_rng().fill_bytes(&mut random[..]);
 
-  bench_group(c, "adler", &random, |data| {
-    let mut adler = adler::Adler32::new();
-
-    adler.write_slice(data);
-    adler.checksum()
-  });
-
-  bench_group(c, "adler32", &random, |data| {
-    let mut adler = adler32::RollingAdler32::new();
-
-    adler.update_buffer(data);
-    adler.hash()
-  });
-
-  bench_group(c, "simd-adler32", &random, |data| {
-    let mut adler = simd_adler32::Adler32::new();
-
-    adler.update(data);
-    adler.finalize()
-  });
-}
-
-fn bench_group<F>(c: &mut Criterion, name: &str, data: &[u8], imp: F)
-where
-  F: Copy + FnOnce(&[u8]) -> u32,
-{
-  assert_eq!(data.len(), 100_000);
-
-  c.benchmark_group(name)
+  c.benchmark_group("10k")
     .throughput(Throughput::Bytes(10_000))
-    .bench_with_input("10k", &data[..10_000], |b, data| {
-      b.iter(|| black_box(imp(data)))
+    .bench_with_input("adler", &random[..10_000], |b, data| {
+      b.iter(|| {
+        let mut adler = adler::Adler32::new();
+
+        adler.write_slice(data);
+        adler.checksum()
+      })
     })
-    //
-    .throughput(Throughput::Bytes(data.len() as _))
-    .bench_with_input("100k", &data[..100_000], |b, data| {
-      b.iter(|| black_box(imp(data)))
+    .bench_with_input("adler32", &random[..10_000], |b, data| {
+      b.iter(|| {
+        let mut adler = adler32::RollingAdler32::new();
+
+        adler.update_buffer(data);
+        adler.hash()
+      })
+    })
+    .bench_with_input("simd-adler32", &random[..10_000], |b, data| {
+      b.iter(|| {
+        let mut adler = simd_adler32::Adler32::new();
+
+        adler.update(data);
+        adler.finalize()
+      })
+    });
+
+  c.benchmark_group("100k")
+    .throughput(Throughput::Bytes(100_000))
+    .bench_with_input("adler", &random[..100_000], |b, data| {
+      b.iter(|| {
+        let mut adler = adler::Adler32::new();
+
+        adler.write_slice(data);
+        adler.checksum()
+      })
+    })
+    .bench_with_input("adler32", &random[..100_000], |b, data| {
+      b.iter(|| {
+        let mut adler = adler32::RollingAdler32::new();
+
+        adler.update_buffer(data);
+        adler.hash()
+      })
+    })
+    .bench_with_input("simd-adler32", &random[..100_000], |b, data| {
+      b.iter(|| {
+        let mut adler = simd_adler32::Adler32::new();
+
+        adler.update(data);
+        adler.finalize()
+      })
     });
 }
 
