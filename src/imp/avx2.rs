3a9838f5d8ebe7f335f7f1181cbac69c06ddf0fb
Marvin Countryman <me@maar.vin>
2021-04-08 12:45:55 -0400

    fix(avx2): fix hashing zeros

--- src/imp/avx2.rs
+++ src/imp/avx2.rs
@@ -3,20 +3,20 @@ const CHUNK_SIZE: usize = 5504; // 5552;
 
 use super::Adler32Imp;
 
-#[cfg(target_arch = "x64")]
-use core::arch::x64::*;
+#[cfg(target_arch = "x84")]
+use core::arch::x86::*;
 #[cfg(target_arch = "x86_64")]
 use core::arch::x86_64::*;
 
 /// Resolves update implementation if CPU supports avx2 instructions.
 pub fn get_imp() -> Option<Adler32Imp> {
-  #[cfg(all(feature = "std", target_arch = "x86_64"))]
+  #[cfg(all(feature = "std", target_arch = "x86"))]
   if std::is_x86_feature_detected!("avx2") {
     return Some(update);
   }
 
-  #[cfg(all(feature = "std", target_arch = "x64"))]
-  if std::is_x64_feature_detected!("avx2") {
+  #[cfg(all(feature = "std", target_arch = "x86_64"))]
+  if std::is_x86_feature_detected!("avx2") {
     return Some(update);
   }
 
@@ -51,7 +51,14 @@ unsafe fn update_imp(a: u16, b: u16, data: &[u8]) -> (u16, u16) {
 }
 
 unsafe fn update_chunk_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
-  debug_assert!(chunk.len() < CHUNK_SIZE);
+  debug_assert_eq!(
+    chunk.len(),
+    CHUNK_SIZE,
+    "Unexpected chunk size (expected {}, got {})",
+    CHUNK_SIZE,
+    chunk.len()
+  );
+
   reduce_add_blocks(a, b, chunk);
 
   *a %= MOD;
@@ -59,55 +66,101 @@ unsafe fn update_chunk_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
 }
 
 unsafe fn update_block(a: &mut u32, b: &mut u32, chunk: &[u8]) {
-  let remainder = reduce_add_blocks(a, b, chunk);
+  debug_assert!(
+    chunk.len() <= CHUNK_SIZE,
+    "Unexpected chunk size (expected <= {}, got {})",
+    CHUNK_SIZE,
+    chunk.len()
+  );
 
-  for byte in remainder {
+  for byte in reduce_add_blocks(a, b, chunk) {
     *a += *byte as u32;
     *b += *a;
   }
 
-  *a %= MOD;
+  if *a >= MOD {
+    *a -= MOD;
+  }
+
   *b %= MOD;
 }
 
 #[inline(always)]
 unsafe fn reduce_add_blocks<'a>(a: &mut u32, b: &mut u32, chunk: &'a [u8]) -> &'a [u8] {
+  if chunk.len() < 64 {
+    return chunk;
+  }
+
   let blocks = chunk.chunks_exact(64);
   let blocks_remainder = blocks.remainder();
 
-  let mut a_v = _mm256_setzero_si256();
-  let mut b_v = a_v;
+  let n = CHUNK_SIZE / 64;
+  let n = if n > blocks.len() {
+    blocks.len() as u32
+  } else {
+    n as u32
+  };
 
-  // let one_v = _mm256_set1_epi16(1);
-  let zero_v = a_v;
-  // let weight_hi_v = get_weight_hi();
-  // let weight_lo_v = get_weight_lo();
+  let one_v = _mm256_set1_epi16(1);
+  let zero_v = _mm256_set1_epi16(0);
+  let weight_hi_v = get_weight_hi();
+  let weight_lo_v = get_weight_lo();
+
+  let mut p_v = _mm256_set_epi32(0, 0, 0, 0, 0, 0, 0, (*a * n) as _);
+  let mut a_v = _mm256_set_epi32(0, 0, 0, 0, 0, 0, 0, 0);
+  let mut b_v = _mm256_set_epi32(0, 0, 0, 0, 0, 0, 0, *b as _);
 
   for block in blocks {
     let block_ptr = block.as_ptr() as *const _;
-    let left_v = _mm256_lddqu_si256(block_ptr);
-    let right_v = _mm256_lddqu_si256(block_ptr.add(1));
+    let left_v = _mm256_loadu_si256(block_ptr);
+    let right_v = _mm256_loadu_si256(block_ptr.add(1));
+
+    p_v = _mm256_add_epi32(p_v, a_v);
 
     a_v = _mm256_add_epi32(a_v, _mm256_sad_epu8(left_v, zero_v));
+    let mad = _mm256_maddubs_epi16(left_v, weight_hi_v);
+    b_v = _mm256_add_epi32(b_v, _mm256_madd_epi16(mad, one_v));
+
     a_v = _mm256_add_epi32(a_v, _mm256_sad_epu8(right_v, zero_v));
-    b_v = _mm256_add_epi32(b_v, a_v);
+    let mad = _mm256_maddubs_epi16(right_v, weight_lo_v);
+    b_v = _mm256_add_epi32(b_v, _mm256_madd_epi16(mad, one_v));
   }
 
+  b_v = _mm256_add_epi32(b_v, _mm256_slli_epi32(p_v, 6));
+
   *a += reduce_add(a_v);
-  *b += reduce_add(b_v);
+  *b = reduce_add(b_v);
 
   blocks_remainder
 }
 
 #[inline(always)]
 unsafe fn reduce_add(v: __m256i) -> u32 {
-  let sum_128 = _mm_add_epi32(_mm256_castsi256_si128(v), _mm256_extracti128_si256(v, 1));
-  let hi_64 = _mm_unpackhi_epi64(sum_128, sum_128);
-  let sum_64 = _mm_add_epi32(hi_64, sum_128);
-  let hi_32 = _mm_shuffle_epi32(sum_64, _MM_SHUFFLE(2, 3, 0, 1));
-  let sum_32 = _mm_add_epi32(sum_64, hi_32);
+  // print_m256i_u32("v", &v);
+  let sum = _mm_add_epi32(_mm256_castsi256_si128(v), _mm256_extracti128_si256(v, 1));
+  // print_m128i_u32("sum", &sum);
+  let hi = _mm_unpackhi_epi64(sum, sum);
+  // print_m128i_u32("hi", &hi);
 
-  _mm_cvtsi128_si32(sum_32) as _
+  let sum = _mm_add_epi32(hi, sum);
+  // print_m128i_u32("sum", &sum);
+  let hi = _mm_shuffle_epi32(sum, _MM_SHUFFLE(2, 3, 0, 1));
+  // print_m128i_u32("hi", &hi);
+
+  let sum = _mm_add_epi32(sum, hi);
+  // print_m128i_u32("sum", &sum);
+  let sum = _mm_cvtsi128_si32(sum) as _;
+  // println!("sum: {}", sum);
+
+  sum
+}
+
+#[inline(always)]
+unsafe fn get_weight_lo() -> __m256i {
+  _mm256_set_epi8(
+    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
+    24, 25, 26, 27, 28, 29, 30, 31,
+  )
 }
 
 #[inline(always)]
@@ -118,49 +171,72 @@ unsafe fn get_weight_hi() -> __m256i {
   )
 }
 
-#[inline(always)]
-unsafe fn get_weight_lo() -> __m256i {
-  _mm256_set_epi8(
-    0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
-    24, 25, 26, 27, 28, 29, 30, 21,
-  )
+unsafe fn print_m256i_u32(s: &str, v: &__m256i) {
+  let v: [u32; 8] = core::mem::transmute(*v);
+  println!("{}: {:?}", s, v);
+}
+
+unsafe fn print_m128i_u16(s: &str, v: &__m128i) {
+  let v: [u16; 8] = core::mem::transmute(*v);
+  println!("{}: {:?}", s, v);
+}
+
+unsafe fn print_m128i_u32(s: &str, v: &__m128i) {
+  let v: [u32; 4] = core::mem::transmute(*v);
+  println!("{}: {:?}", s, v);
+}
+
+unsafe fn print_m128i_u64(s: &str, v: &__m128i) {
+  let v: [u64; 2] = core::mem::transmute(*v);
+  println!("{}: {:?}", s, v);
 }
 
 #[cfg(test)]
 mod tests {
+  use rand::Rng;
+
   #[test]
   fn zeroes() {
-    assert_eq!(adler32(&[]), 1, "len(0)");
-    assert_eq!(adler32(&[0]), 1 | 1 << 16, "len(1)");
-    assert_eq!(adler32(&[0, 0]), 1 | 2 << 16, "len(2)");
-    assert_eq!(adler32(&[0; 100]), 0x00640001, "len(100)");
-    assert_eq!(adler32(&[0; 1024]), 0x04000001, "len(1k)");
-    assert_eq!(adler32(&[0; 1024 * 1024]), 0x00f00001, "len(1m)");
+    assert_sum_eq(&[]);
+    assert_sum_eq(&[0]);
+    assert_sum_eq(&[0, 0]);
+    assert_sum_eq(&[0; 100]);
+    assert_sum_eq(&[0; 1024]);
+    assert_sum_eq(&[0; 1024 * 1024]);
   }
 
   #[test]
   fn ones() {
-    assert_eq!(adler32(&[0xff; 1024]), 0x79a6fc2e, "len(1k)");
-    assert_eq!(adler32(&[0xff; 1024 * 1024]), 0x8e88ef11, "len(1m)");
+    assert_sum_eq(&[]);
+    assert_sum_eq(&[1]);
+    assert_sum_eq(&[1, 1]);
+    assert_sum_eq(&[1; 100]);
+    assert_sum_eq(&[1; 1024]);
+    assert_sum_eq(&[1; 1024 * 1024]);
   }
 
   #[test]
-  fn mixed() {
-    assert_eq!(adler32(&[1]), 2 | 2 << 16, "len(1)");
-    assert_eq!(adler32(&[40]), 41 | 41 << 16, "len(40)");
+  fn random() {
+    let mut random = vec![0; 1024 * 1024];
+    rand::thread_rng().fill(&mut random[..]);
 
-    assert_eq!(adler32(&[0xA5; 1024 * 1024]), 0xd5009ab1, "len(1m)");
+    assert_sum_eq(&random[..1]);
+    assert_sum_eq(&random[..100]);
+    assert_sum_eq(&random[..1024]);
+    assert_sum_eq(&random[..1024 * 1024]);
   }
 
   /// Example calculation from https://en.wikipedia.org/wiki/Adler-32.
   #[test]
   fn wiki() {
-    assert_eq!(adler32(b"Wikipedia"), 0x11E60398);
+    assert_sum_eq(b"Wikipedia");
   }
 
-  fn adler32(data: &[u8]) -> u32 {
+  fn assert_sum_eq(data: &[u8]) {
     let (a, b) = super::update(1, 0, data);
+    let left = u32::from(b) << 16 | u32::from(a);
+    let right = adler::adler32_slice(data);
 
-    u32::from(b) << 16 | u32::from(a)
+    assert_eq!(left, right, "len({})", data.len());
   }
 }
