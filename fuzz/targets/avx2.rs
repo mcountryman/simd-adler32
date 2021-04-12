691731e249525378f9d13bf4fc3edc4377f0ddc7
Marvin Countryman <me@maar.vin>
2021-04-12 10:24:49 -0400

    ci(fuzz): fix syntax error and add cache

--- fuzz/targets/avx2.rs
+++ fuzz/targets/avx2.rs
@@ -1,11 +1,9 @@
 #![no_main]
 #[macro_use]
-use libfuzzer_sys::fuzz_target;
+extern crate libfuzzer_sys;
 
-fuzz_target! {
-  |data: &[u8] {
-    if let Some(imp) = simd_adler32::imp::avx2::get_imp() {
-      imp(1, 0, data);
-    }
+fuzz_target! (|data: &[u8] {
+  if let Some(imp) = simd_adler32::imp::avx2::get_imp() {
+    imp(1, 0, data);
   }
-}
+});
